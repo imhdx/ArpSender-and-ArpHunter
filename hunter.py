997171e9ad7aea441d56d010b57dd94d653952f7
imhdx <mhdxacmer@126.com>
2020-01-05 22:51:28 +0800

    update 2020/1/5

---
+++ hunter.py
@@ -0,0 +1,110 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from scapy.all import *
+
+
+def get_if_name():  # 获取所有可用网卡的name
+    guids = get_if_list()  # 获取所有可用网卡的guid
+    if_list = get_windows_if_list()  # 获取windows上所有网卡的所有信息
+    names = []  # 存储所有可用网卡的name
+    for guid in guids:
+        for _if in if_list:  # 双重循环扫描
+            if _if['guid'] in guid:
+                names.append(_if['name'])
+                break
+    return names
+
+
+class App:  # 界面设计
+    def __init__(self, master):
+        self.master = master
+        self.nowif = tk.StringVar()
+        self.thread = None
+        self.stbu = None
+        self.now = 0.0
+        self.num = 1
+        self.tree = None
+        self.initWidgets()
+        self.master.mainloop()
+
+    def initWidgets(self):
+        ttk.Label(self.master, text='选择网卡').place(x=20, y=10)
+        com = ttk.Combobox(self.master, textvariable=self.nowif, width=90)
+        com['value'] = get_if_name()
+        com.current(0)
+        com.config(state='readonly')
+        com.place(x=100, y=10)
+        self.stbu = ttk.Button(self.master, text='开始', command=self.start)
+        self.tree.column('长度(B)', width=55, anchor='center')
+        self.tree.column('源IP地址', width=110, anchor='w')
+        self.tree.column('源MAC地址', width=110, anchor='w')
+        self.tree.column('目的IP地址', width=110, anchor='w')
+        self.tree.column('目的MAC地址', width=110, anchor='w')
+        self.stbu.place(x=25, y=50)
+        ttk.Button(self.master, text='停止', command=self.stop).place(x=236, y=50)
+        ttk.Button(self.master, text='清除所有', command=self.clear).place(x=447, y=50)
+        ttk.Button(self.master, text='退出', command=self.quit).place(x=658, y=50)
+
+        self.tree = ttk.Treeview(self.master, height=10,
+                                 column=('编号', '协议类型', '时间', '长度(B)', '源IP地址', '源MAC地址', '目的IP地址', '目的MAC地址'))
+        self.tree.column('编号', width=50, anchor='center')
+        self.tree.column('协议类型', width=80, anchor='center')
+        self.tree.column('时间', width=90, anchor='center')
+        self.tree.heading('编号', text='编号')
+        self.tree.heading('协议类型', text='协议类型')
+        self.tree.heading('时间', text='时间')
+        self.tree.heading('长度(B)', text='长度(B)')
+        self.tree.heading('源IP地址', text='源IP地址')
+        self.tree.heading('源MAC地址', text='源MAC地址')
+        self.tree.heading('目的IP地址', text='目的IP地址')
+        self.tree.heading('目的MAC地址', text='目的MAC地址')
+        self.tree.config(show='headings')
+        self.tree.place(x=25, y=90)
+
+        vbar = ttk.Scrollbar(self.master, orient='vertical', command=self.tree.yview)
+        self.tree.configure(yscrollcommand=vbar.set)
+        vbar.place(x=743, y=89, height=230)
+
+    def insert_data(self, data):  # 向表格中插入数据
+        if self.now == 0.0:  # 从抓到的第一个包开始计时
+            self.now = time.clock()
+        nowtime = time.clock() - self.now
+        nowtime = '%.6f' % nowtime
+        arp_type = 'who_is' if data.op == 1 else 'is_at'  # 判断抓到的包的类型
+        self.tree.insert('', 'end',
+                         values=(self.num, arp_type, nowtime, len(data), data.psrc, data.hwsrc, data.pdst, data.hwdst))
+        self.num += 1  # 包的编号加一
+
+    def sniffing(self):  # 抓包核心函数
+        self.thread = AsyncSniffer(count=0, filter='arp', iface=self.nowif.get(), prn=self.insert_data)
+
+    def start(self):  # 开始抓包
+        self.clear()  # 清空表格
+        self.now = 0.0  # 初始化抓包的时间为0
+        self.num = 1  # 初始化包的编号为1
+        if self.thread is not None:  # 若正在抓包，则先停止抓包
+            self.stop()
+        self.sniffing()  # 调用抓包核心函数
+        self.thread.start()  # 启动线程
+        self.stbu.config(state='disabled')  # 使开始按钮变得不可点击
+
+    def stop(self):  # 停止抓包
+        if self.thread is not None:  # 若正在抓包，则停止抓包
+            self.thread.stop()
+            self.thread = None
+        self.stbu.config(state='normal')  # 使开始变得可点击
+
+    def clear(self):  # 清空表格
+        for i in self.tree.get_children():  # 循环表格的每个儿子并删除
+            self.tree.delete(i)
+
+    def quit(self):  # 退出程序
+        self.stop()  # 先停止抓包
+        exit(0)  # 再退出
+
+
+master = tk.Tk()
+master.title('ArpHunter')
+master.geometry('790x400+405+200')
+master.resizable(False, False)
+App(master)
---
+++ sender.py
@@ -0,0 +1,129 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.messagebox
+from scapy.all import *
+
+
+def get_if_name():  # 获取所有可用网卡的name
+    guids = get_if_list()  # 获取所有可用网卡的guid
+    if_list = get_windows_if_list()  # 获取windows上所有网卡的所有信息
+    names = []  # 存储所有可用网卡的name
+    for guid in guids:
+        for _if in if_list:  # 双重循环扫描
+            if _if['guid'] in guid:
+                names.append(_if['name'])
+                break
+    return names
+
+
+def is_ip(ip):  # 判断ip是否为合法ip
+    try:
+        nums = ip.split('.')  # 取出ip的四个段，并判断是不是都是[0,255]的范围
+        trueSum = 0
+        for i in nums:
+            if 0 <= int(i, 10) <= 255:
+                trueSum += 1
+        if trueSum == 4:
+            return True
+        return False
+    except:
+        return False
+
+
+def is_mac(mac):  # 判断mac是否为合法mac
+    try:
+        hexs = mac.split(':')  # 取出mac的六个段，并判断是不是都是[0,255]的范围
+        if len(hexs) != 6:
+            hexs = mac.split('-')
+        trueSum = 0
+        for i in hexs:
+            if 0 <= int(i, 16) <= 255:
+                trueSum += 1
+        if trueSum == 6:
+            return True
+        return False
+    except:
+        return False
+
+
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.op = tk.IntVar()
+        self.op.set(1)
+        self.nowif = tk.StringVar()
+        self.hwsrc = tk.StringVar()
+        self.psrc = tk.StringVar()
+        self.hwdst = tk.StringVar()
+        self.pdst = tk.StringVar()
+        self.info = None
+        self.initWidgets()
+        self.master.mainloop()
+
+    def initWidgets(self):
+        ttk.Label(self.master, text='选择网卡').place(x=20, y=10)
+        com = ttk.Combobox(self.master, textvariable=self.nowif, width=51)
+        com['value'] = get_if_name()
+        com.current(0)
+        com.config(state='readonly')
+        com.place(x=125, y=10)
+        ttk.Label(self.master, text='目的MAC地址').place(x=20, y=50)
+        ttk.Entry(self.master, textvariable=self.hwdst).place(x=125, y=50)
+        ttk.Label(self.master, text='目的IP地址').place(x=20, y=90)
+        ttk.Entry(self.master, textvariable=self.pdst).place(x=125, y=90)
+        ttk.Label(self.master, text='源MAC地址').place(x=20, y=130)
+        ttk.Entry(self.master, textvariable=self.hwsrc).place(x=125, y=130)
+        ttk.Label(self.master, text='源IP地址').place(x=20, y=170)
+        ttk.Entry(self.master, textvariable=self.psrc).place(x=125, y=170)
+        ttk.Label(self.master, text='报文类型').place(x=20, y=210)
+        ttk.Radiobutton(self.master, variable=self.op, text='请求报文', value=1).place(x=125, y=210)
+        ttk.Radiobutton(self.master, variable=self.op, text='应答报文', value=2).place(x=200, y=210)
+        self.info = tk.Text(self.master, textvariable=self.info, width=29, height=11)
+        self.info.place(x=300, y=50)
+        ttk.Button(self.master, text='发送', command=self.mysend).place(x=306, y=208)
+        ttk.Button(self.master, text='退出', command=self.myquit).place(x=415, y=208)
+
+    def myquit(self):  # 退出函数
+        exit(0)
+
+    def mysend(self):  # 发送函数
+        try:
+            pkt = ARP(op=self.op.get(), hwlen=6, plen=4)  # 构造ARP包
+            if is_ip(self.pdst.get()):
+                pkt.pdst = self.pdst.get()
+            if is_ip(self.psrc.get()):
+                pkt.psrc = self.psrc.get()
+            if is_mac(self.hwdst.get()):
+                pkt.hwdst = self.hwdst.get()
+            if is_mac(self.hwsrc.get()):
+                pkt.hwsrc = self.hwsrc.get()
+            pkt.show()
+            send(pkt, iface=self.nowif.get())  # 从网卡iface发送ARP包
+            self.pdst.set(pkt.pdst)  # 更新输入框的值
+            self.psrc.set(pkt.psrc)
+            self.hwdst.set(pkt.hwdst)
+            self.hwsrc.set(pkt.hwsrc)
+            self.info.insert('end', 'Send 1 packets.\n')  # 更新文本框
+            res = '###[ ARP ]### \n'
+            res += '  hwtype = 0x1\n'
+            res += '  ptype  = IPv4\n'
+            res += '  hwlen  = 6\n'
+            res += '  plen   = 4\n'
+            if self.op.get() == 1:
+                res += '  op     = who-has\n'
+            else:
+                res += '  op     = is-at\n'
+            res += '  hwdst  = ' + pkt.hwdst + '\n'
+            res += '  pdst   = ' + pkt.pdst + '\n'
+            res += '  hwsrc  = ' + pkt.hwsrc + '\n'
+            res += '  psrc   = ' + pkt.psrc + '\n'
+            self.info.insert('end', res + '\n')
+        except:  # 若有异常被抛出，则弹窗警告
+            tk.messagebox.showinfo(parent=self.master, title='提示', message='发生错误，请检查各项参数')
+
+
+master = tk.Tk()
+master.title('ArpSender')
+master.geometry('542x255+458+200')
+master.resizable(False, False)
+App(master)
